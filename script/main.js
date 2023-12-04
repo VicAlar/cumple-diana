81a21b8a7fe2ade59100ba18206da999541d0e1a
Victor <valfonsob98@gmail.com>
2023-12-03 20:43:12 -0400

    Code

--- script/main.js
+++ script/main.js
@@ -28,7 +28,7 @@ const video = document.getElementById("video");
 video.addEventListener("canplay", function() {
     setTimeout(function() {
         video.play();
-    }, 5000);
+    }, 38000);
 });
 
 
